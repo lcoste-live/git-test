10785699d27e1717212507e920140bb1e569b3be
COSTE <lcoste@live.fr>
2022-10-19 13:49:44 +0200

    USER1 V16 M2

--- code.cs
+++ code.cs
@@ -6,8 +6,8 @@ public namespace GitTest
         var version = "4.0.0.16";
         var codeSpecifiqueV16 = "Mon Code V16";
         var maModifUser2 = "USER2";
-        var maModifUser1 = "USER1";
-        var maModifCommune = "COMMUN";
+        var maModifUser1 = "USER1 M2";
+        var maModifCommune = "COMMUN USER1 M2";
         var message = $"Version {version}";
 
         this.Show(message);
