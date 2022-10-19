44b32d8dd3b1d2f21aa9b8ab483a168c2e5be628
COSTE <lcoste@live.fr>
2022-10-19 13:37:49 +0200

    Commit V16 USER2

--- code.cs
+++ code.cs
@@ -5,6 +5,8 @@ public namespace GitTest
     {
         var version = "4.0.0.16";
         var codeSpecifiqueV16 = "Mon Code V16";
+        var maModifUser2 = "USER2";
+        var maModifCommune = "COMMUN";
         var message = $"Version {version}";
 
         this.Show(message);
