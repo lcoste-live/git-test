faafa76b62f9f7a030d10efaa0e6294fdc7849e2
COSTE <lcoste@live.fr>
2022-10-19 13:10:07 +0200

    Initial commit

---
+++ code.cs
@@ -0,0 +1,17 @@
+// Version 4.0.0.15
+public namespace GitTest
+{
+    public void main() 
+    {
+        var version = "4.0.0.15";
+        var message = $"Version {version}";
+
+        this.Show(message);
+        
+    }
+
+    public void Show(string message)
+    {
+
+    }
+}
